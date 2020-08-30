86c6ed289cec5c8d9639325e7795e5cecdbb9484
Luma Montes <lumagoesmontes@gmail.com>
2020-08-30 20:31:35 -0300

    finalizado validacao de email

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
 const Input = document.querySelector('input[type=text]')
 const form = document.querySelector('form')
+const borda = document.querySelector('fieldset')
+const icon = document.querySelector('.fas')
 
 Input.addEventListener('blur', (event) => {
     var info = form.email.value
-
-    if(info.indexOf("@ ") >= 0) {
+    if (info.indexOf("@") >= 0) {
         var email = info.split("@")
         if (email[1].indexOf('.') < 0) {
-            console.log('complete seu e-mail corretamente')
+
+            borda.style.borderColor = '#FF5757'
+            icon.style.color = '#FF5757'
+
+            var lista = document.createElement('p')
+            var textolista = document.createTextNode('Por favor, insira um e-mail válido.')
+            lista.appendChild(textolista)
+
+            var divinput = document.querySelector('.inputemail');
+            divinput.appendChild(lista)
+            
+            divinput.style.margin = '10px'
+
+            divinput.classList.remove('inputemail')
+
         }
     }
-    console.log(email)
 })
